0e505f50a3f5e045ca54ca015eba770f34a01794
yousan <yousan@l2tp.org>
2022-03-06 00:36:46 +0900

    refactor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-// import logo from './logo.svg';
 import './App.css';
 import {React, useState} from 'react'
-import { unmountComponentAtNode, render } from "react-dom";
-
 import { Wheel } from 'react-custom-roulette'
 
 
